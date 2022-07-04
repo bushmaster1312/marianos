6471c5ae70d6a51d8dbe7c9d96463b2159fb65bd
bushmaster1312 <marianodominguez1992@gmail.com>
2022-07-04 19:35:31 -0300

    ya valio madrecasss

--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -10,36 +10,36 @@ const Servicios = () => {
     useEffect(() => {
         getServicios(`${BASE_URL}/servicios`)
     }, [])
-   
+
 
 
     return (
-        <div className='servicios-home col-12'>
-            <div className='  servicios-contenedor ' id="servicioshome">
-                <div className='row'>
-                    {
-                        servicios.length === 0 ?
-                            <div className='loading-contenedor'>
-                                <button className="boton-loading" type="button" disabled>
-                                    <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                                    Cargando...
-                                </button>
-                            </div>
-                            :
-                            servicios.map((servicio, idx) => (
-                                <div className=' col-sm-12 col-md-6 col-lg-4 contenedor-cards mt-4' key={idx} >
-                                    <div className="cards-homes" >
-                                        <img src={`${BASE_URL}/servicios/aws/imagen/${servicio.imagen}`} alt="sdasd" />
-                                        <div className='boton-servicios'>
-                                            <Link to={`${servicio.categorias}`}><button>Mostrar Mas</button>    </Link>
-                                        </div>
-                                    </div>
 
-                                </div>))
-                    }
-                </div>
+        <div className=' servicios-contenedor ' id="servicioshome">
+            <div className='contenedor-serv'>
+                {
+                    servicios.length === 0 ?
+                        <div className='loading-contenedor'>
+                            <button className="boton-loading" type="button" disabled>
+                                <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+                                Cargando...
+                            </button>
+                        </div>
+                        :
+                        servicios.map((servicio, idx) => (
+                            <div className='cardss-contenedor ' key={idx} >
+                                <div className="cards-home" >
+                                    <img className='imagen' src={`${BASE_URL}/servicios/aws/imagen/${servicio.imagen}`} alt="sdasd" />
+                                    <div className='boton-servicios'>
+                                        <Link to={`${servicio.categorias}`}><button>Mostrar Mas</button>    </Link>
+                                    </div>
+                                </div>
+
+                            </div>))
+                }
             </div>
-        </div >
+        </div>
+
     )
 }
 
