5d516cabc8c1bbdb7cce96c4871e037290255495
bushmaster1312 <marianodominguez1992@gmail.com>
2022-07-10 18:11:44 -0300

    me tesss

--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
 import { Context } from '../../store/AppContext'
 import { BASE_URL } from '../../utils/BASE_URL'
 import "./Servicios.css"
+import loreal from "../../assets/iconos/loreal.svg"
 
 const Servicios = () => {
     const { servicios, getServicios } = useContext(Context)
@@ -14,30 +14,36 @@ const Servicios = () => {
 
 
     return (
+        <div>
+            <div className='  servicios-contenedor  ' id="servicioshome">
 
-        <div className='  servicios-contenedor  ' id="servicioshome">
-
-            {
-                servicios.length === 0 ?
-                    <div className='loading-contenedor'>
-                        <button className="boton-loading" type="button" disabled>
-                            <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                            Cargando...
-                        </button>
-                    </div>
-                    :
-                    servicios.map((servicio, idx) => (
-                        <div className="cards-home " key={idx} >
-                            <img className='imagen' src={`${BASE_URL}/servicios/aws/imagen/${servicio.imagen}`} alt="sdasd" />
-                            <p>{servicio.categorias} </p>
-                            <div className='boton-servicios'>
-                                <Link to={`${servicio.categorias}`}><button>Mostrar Mas</button>    </Link>
-                            </div>
+                {
+                    servicios.length === 0 ?
+                        <div className='loading-contenedor'>
+                            <button className="boton-loading" type="button" disabled>
+                                <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+                                Cargando...
+                            </button>
                         </div>
+                        :
+                        servicios.map((servicio, idx) => (
+                            <div className="cards-home " key={idx} >
+                                <img className='imagen' src={`${BASE_URL}/servicios/aws/imagen/${servicio.imagen}`} alt="sdasd" />
+                                <p>{servicio.categorias} </p>
+                                <div className='boton-servicios'>
+                                    <Link to={`${servicio.categorias}`}><button>Mostrar Mas</button>    </Link>
+                                </div>
+                            </div>
 
-                    ))
-            }
-        </div>
+                        ))
+                }
+
+
+            </div>
+            <div className=' productos'>
+                <img src={loreal} alt="sss" />
+            </div>
+            </div>
 
 
     )
